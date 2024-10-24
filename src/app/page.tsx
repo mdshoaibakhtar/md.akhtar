3e02711c56b08facbce183558a3ac492780e98b2
Md Shoaib Akhtar <mdshoaibakhtar966@gmail.com>
2024-10-24 17:36:12 +0530

    Added footer and fixed the width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import Dashboard from "@/components/Dashboard";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="bg-black">
               <Navbar/>
               <Dashboard/>
               <Skills/>
               <Experience/>
+              <Footer/>
     </div>
   );
 }
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,10 +5,10 @@ import performance from '../images/performance.png'
 
 export default function Skills() {
     return (
-      <div className="bg-gray-50 py-24 sm:py-32">
+      <div className="bg-black py-24 sm:py-32">
         <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
-          <h2 className="text-center text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
-          <p className="mx-auto mt-2 max-w-lg text-balance text-center text-4xl font-semibold tracking-tight text-gray-950 sm:text-5xl">
+          <h2 className="text-center text-base/7 font-semibold text-gray-100">Deploy faster</h2>
+          <p className="mx-auto mt-2 max-w-lg text-balance text-center text-4xl font-semibold tracking-tight text-gray-100 sm:text-5xl">
             Everything you need to deploy your app
           </p>
           <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
