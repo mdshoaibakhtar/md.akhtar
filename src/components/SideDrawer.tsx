54e507e590edac8f1f76b2919f039d5f4d8315c0
Md Shoaib Akhtar <mdshoaibakhtar966@gmail.com>
2024-10-27 22:58:11 +0530

    align items center

--- src/components/SideDrawer.tsx
+++ src/components/SideDrawer.tsx
@@ -15,10 +15,10 @@ const SideDrawer: React.FC<DrawerProps> = ({ open, setOpen }) => {
             />
 
             <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-                <div className="flex justify-center p-4 text-center sm:items-center sm:p-0">
+                <div className="flex justify-center items-center p-4 text-center sm:items-center sm:p-0">
                     <DialogPanel
                         transition
-                        className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-1 w-9/12 sm:w-full sm:max-w-2xl data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+                        className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-1 w-10/12 sm:w-full sm:max-w-2xl data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
                     >
                         <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                             <div className="sm:flex sm:items-start">
