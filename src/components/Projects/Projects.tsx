b5ba1405bc601c86cd5025e01aa5bcd3ca320560
Md Shoaib <mdshoaibakhtar966@gmail.com>
2025-03-13 00:01:56 +0530

    Update content

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -10,14 +10,8 @@ const Projects = () => {
         description: "Find your dream job with Jobdotjs",
         tech_stack: ["Nextjs", "Django Rest Framework", "PostgreSQL", "TailwindCSS", "Vercel"],
         status: "Available",
-        other_features: ["Separate UI for Job seeker & Hiring talent", "Hiring talent - Job posting", "All type of Job searches", "Easy apply jobs", "AI Chatbot"],
-        features: [
-            "Created a job/employment web application with separate access for companies/HR (admin) and employees (standard users) to manage job applications and postings.",
-            "Implemented features for job seekers to easily apply for job roles and for admins to post job openings, streamlining the hiring process.",
-            "Utilized Nextjs for the front end and Django REST Framework (DRF) for building robust APIs, along with PostgreSQL as the database.",
-            "Currently integrating a new React Flow feature to streamline the hiring process, with plans to implement an AI chatbot for assistance and support.",
-            "Project is currently in progress with ongoing feature enhancements and optimizations."
-        ],
+        other_features: ["Role-Based Access Control", "Hiring talent - Job posting", "All type of Job searches", "Easy apply jobs", "AI Chatbot"],
+        features: [],
         live_link: "https://jobdotjs.vercel.app/",
         github_link: "https://github.com/mdshoaibakhtar/Job.js"
     }
@@ -28,11 +22,7 @@ const Projects = () => {
         tech_stack: ["Python", "Django Rest Framework", "PostgreSQL", "AWS", "GraphQL", "AppSync", "VPC", "Relational Database", "Secrets Manager", "Lambda"],
         status: "Available",
         other_features: ["Cross platform integration", "GraphQL API development", "Data migration", "Boomi platform integration", "Utilization of aws services"],
-        features: [
-            "Led cross-platform integration projects, focusing on middleware development to enable ERP data migration from an old database to AWS RDS.",
-            "Designed and implemented private GraphQL APIs using AppSync, VPC, Relational Database, and Secrets Manager.",
-            "Integrated these private GraphQL APIs using the Boomi platform, enabling seamless connectivity and efficient data handling across systems."
-        ],
+        features: [],
         live_link: "",
         github_link: ""
     }
@@ -51,11 +41,12 @@ const Projects = () => {
         title: "Jobdotjs",
         description: "Find your dream job with Jobdotjs",
         features: [
-            "Created a job/employment web application with separate access for companies/HR (admin) and employees (standard users) to manage job applications and postings.",
+            "Created a job web application with separate access for companies/HR (admin) and employees to manage job applications and postings.",
             "Implemented features for job seekers to easily apply for job roles and for admins to post job openings, streamlining the hiring process.",
             "Utilized Nextjs for the front end and Django REST Framework (DRF) for building robust APIs, along with PostgreSQL as the database.",
             "Currently integrating a new React Flow feature to streamline the hiring process, with plans to implement an AI chatbot for assistance and support.",
-            "Project is currently in progress with ongoing feature enhancements and optimizations."
+            "A visual workflow to allow recruiters to drag-and-drop applications through various hiring stages (e.g., Screening → Interview → Offer).",
+            "The project is actively in development, with continuous improvements and optimizations to enhance user experience and platform functionality.."
         ]
     }
 
@@ -63,9 +54,12 @@ const Projects = () => {
         title: "Nicholas",
         description: "Cross-Platform Integration and Middleware Development",
         features: [
-            "Led cross-platform integration projects, focusing on middleware development to enable ERP data migration from an old database to AWS RDS.",
-            "Designed and implemented private GraphQL APIs using AppSync, VPC, Relational Database, and Secrets Manager.",
-            "Integrated these private GraphQL APIs using the Boomi platform, enabling seamless connectivity and efficient data handling across systems."
+            "Led cross-platform integration projects, Implementation of middleware layer that automates data extraction, transformation, and loading (ETL) from the legacy database to AWS RDS.",
+            "Ensured data consistency, integrity, and security while migrating complex ERP datasets across different platforms.",
+            "Optimized database queries and indexing strategies to enhance migration speed and minimize operational disruptions.",
+            "Developed secure, private GraphQL APIs using AWS AppSync to enable real-time, flexible, and efficient data access for ERP systems.",
+            "Implemented fine-grained access control using AWS IAM roles and Secrets Manager, ensuring secure API interactions.",
+            "Configured AWS VPC (Virtual Private Cloud) to keep API communication secure within a private network, restricting unauthorized access."
         ]
     }
 
@@ -73,9 +67,12 @@ const Projects = () => {
         title: "App0",
         description: "AI-powered multiparty messaging platform",
         features: [
-            "Designed and developed an AI-powered multiparty messaging platform, enhancing communication and collaboration by 76% using ReactJS, DRF(Django), and AWSservices such as LambdaFunction, API Gateway, DynamoDB, and Secrets Manager.",
-            "Worked on PaymentGateway and DocumentUpload web app for end users, with a customizable dashboard for bank authorities with full UI configuration, and enabling banks to send sms via Twilio or Pinpoint.",
-            "Integrated AWS WAFCaptcha and Multi-Factor Authentication (MFA) via QR code, improving security by 76%."
+            "Engineered high-performance, scalable web applications using ReactJS, Django REST Framework, and Node.js, ensuring seamless front-end and back-end integration.",
+            "Designed and optimized efficient database architectures using PostgreSQL and DynamoDB, improving query performance and data retrieval speeds.",
+            "Developed highly responsive and interactive UIs with ReactJS and Tailwind CSS, enhancing user experience and increasing engagement rates.",
+            "Implemented server-side rendering (SSR) with Next.js to optimize SEO and improve page load speeds by 40%, leading to better user retention.",
+            "Designed RESTful APIs with Django REST Framework (DRF) and Node.js (Express.js), ensuring seamless integration with front-end applications.",
+            "Result: Improved overall application efficiency, increased product performance by 86%, and enhanced user engagement metrics."
         ]
     }
     return (
